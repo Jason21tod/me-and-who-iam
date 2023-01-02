c83a1bf5a1b109757121d618a0c26fa2ecc4077d
Gian_Pereira <gian.21.otaku@gmail.com>
2023-01-02 18:50:25 -0300

    preparando os novos docs

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 
+var description = document.getElementsByClassName('skills_legend__container');
+console.log(description);
 
+var description = description.item(0);
+console.log(description);
+
+description.style['visibility'] ='hidden';
+description.style['opacity'] = '0%';
+description.style['margin-bottom'] = '-10%';
 
 
 function toggleDescription () {
     
     var button = document.getElementsByClassName('skills_legend__container_icon');
     console.log(button);
-
-    var description = document.querySelector('.skills_legend__container')
-    console.log(description.getAttributeNames())
-
+    
     if (description.style['visibility'] === 'visible') {
         console.log('desligado');
         description.style['visibility'] ='hidden';
@@ -23,4 +28,17 @@ function toggleDescription () {
         description.style['opacity'] = '100%';
         description.style['margin-bottom'] = '10%';
     }
+}
+
+
+function toggleJasonWpp () {
+    var jason_image = document.getElementById('jason_wpp');
+
+    if (jason_image.innerHTML.endsWith('<a class="jason_link" href="#"><img class="jason_image" src="static/css/jason.png" alt="jason logo"></a>')) {
+        var jason_link = jason_image.getElementsByClassName('jason_link');
+        jason_link.item(0).remove();
+    }
+    else {
+        jason_image.insertAdjacentHTML("beforeend", '<a class="jason_link" href="#"><img class="jason_image" src="static/css/jason.png" alt="jason logo"></a>');
+    };
 }
