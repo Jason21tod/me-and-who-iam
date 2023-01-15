9570031f30b0b5c7c02d45187cf16bf73204dd61
Gian_Pereira <gian.21.otaku@gmail.com>
2023-01-15 10:37:20 -0300

    g

--- app.py
+++ app.py
@@ -9,8 +9,4 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def run_app():
-    return app.run(host='127.0.0.1:5500')
 
-if __name__ == '__main__':
-    app.run()
