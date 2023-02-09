8f2e9be825075237c91ae2f564755ddbfe959a9a
Gian_Pereira <gian.21.otaku@gmail.com>
2023-02-09 09:22:40 -0300

    (try to fix)

--- app/msg_handlers.py
+++ app/msg_handlers.py
@@ -51,11 +51,11 @@ class PrimaryMsgReceiver(MsgReceiver):
     """
     def receive_and_response_msg(self, msg) -> messaging_response.MessagingResponse:
         msg_data: dict = _format_request_to_msg_dict(msg)
-        msg = messaging_response.MessagingResponse()
-        message = client.messages.create(body=self.commom_response,
-                            media_url='https://raw.githubusercontent.com/Jason21tod/me-and-who-iam/1bbf64b61024e17bd2037604c11e35ad06e34bd2/app/static/jason_whats_profile.png',
-                            from_=msg_data['to'],
-                            to=msg_data['from'])
+        response = messaging_response.MessagingResponse()
+        message = client.messages.create(body= self.commom_response,
+                    media_url='https://raw.githubusercontent.com/Jason21tod/me-and-who-iam/1bbf64b61024e17bd2037604c11e35ad06e34bd2/app/static/jason_whats_profile.png',
+                    from_= msg_data['to'],
+                    to= msg_data['from'])
         return 'hi, maybe iam not working...'
 
         
