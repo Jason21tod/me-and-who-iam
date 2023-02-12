17c3196a3c2f61bdda5167adeb05423bbd7af6fd
Gian_Pereira <gian.21.otaku@gmail.com>
2023-02-12 12:03:40 -0300

    correcting the bug

--- api/static/scripts/index.js
+++ api/static/scripts/index.js
@@ -4,25 +4,28 @@ const ME_AND_WHO_IAM_BOX = document.getElementById('me_and_who_iam_description-l
 console.log(JASON_BOX);
 
 
+var activation = false;
+
 function alter_box_description(activation_box, description_box) {
     var box = document.getElementById(activation_box);
     var description = document.getElementById(description_box);
     console.log('obtendo -> ', box);
     console.log('obtendo -> ', description);
 
-    console.log('Opacidade', description.style['opacity']);
-    if (description.style['opacity'] == '0') {
-        console.log('ligado');
-        box.style['background'] = 'blue'
-        box.style['scale'] = '1.2'
+    if (activation == false) {
+        console.log('Activation state ', activation, 'Activating...');
+        description.style['transition'] = 'all 1s ease 0s;'
+        box.style['background'] = 'blue';
         description.style['opacity'] = '100';
-        description.style['display'] = 'flex'
-
-    } else {
-        console.log('desligado');
-        box.style['background'] = 'var(--aqua-default)'
-        box.style['scale'] = '1'
+        description.style['display'] = 'flex';
+        activation = true;
+        
+    } else if (activation == true) {
+        console.log('Activation state ', activation, 'off...');
+        description.style['transition'] = 'all 1s ease 0s;'
+        box.style['background'] = 'var(--aqua-default)';
         description.style['opacity'] = '0';
         description.style['display'] = 'none'
+        activation = false
     }
 }
