1d6b8209e96486738f85522373e219adfd3d3d99
Gian_Pereira <gian.21.otaku@gmail.com>
2023-02-19 11:15:27 -0300

    That its all

---
+++ api/app.py
@@ -0,0 +1,30 @@
+import flask 
+from flask import request
+from logging import info
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return flask.render_template('home_page.html')
+
+
+@app.route('/jason_bot.html')
+def jason_bot_page():
+    info('Going to jason page')
+    return flask.render_template('jason_bot.html')
+
+@app.route('/bot', methods=['GET', 'POST'])
+def bot_endpoint()-> str:
+    from .msg_handlers import PrimaryMsgReceiver
+    msg_receiver = PrimaryMsgReceiver()
+    info('Going to bot page')
+    req = request.values
+    return str(msg_receiver.receive_and_response_msg(req))
+
+def run_app():
+    return app
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
