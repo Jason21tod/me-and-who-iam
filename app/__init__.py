d67fa53acbd679595f7c7382a10d3947e7c99ee2
Gian_Pereira <gian.21.otaku@gmail.com>
2023-06-09 11:11:11 -0300

    fixing somethings

--- app/__init__.py
+++ app/__init__.py
@@ -37,8 +37,8 @@ def bot_endpoint()-> str:
 @app.route('/auth/', methods=['GET', 'POST'])
 def auth():
     req = requests.get("""https://api.instagram.com/oauth/authorize
-    ?client_id={1659663484497725}
-    &redirect_uri'={https://jason-todd.herokuapp.com/auth/}
+    ?client_id=1659663484497725
+    &redirect_uri'=https://jason-todd.herokuapp.com/auth/
     &scope=user_profile,user_media
     &response_type=code""")
     return req.text
