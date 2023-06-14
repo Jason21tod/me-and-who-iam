7419f872aa2129525ec91c6ab1c0dc2e1039054c
Gian_Pereira <gian.21.otaku@gmail.com>
2023-06-14 09:48:03 -0300

    add new skills

--- app/__init__.py
+++ app/__init__.py
@@ -38,6 +38,31 @@ def bot_endpoint()-> str:
 def auth():
     return app.redirect("""https://api.instagram.com/oauth/authorize/?redirect_uri=https://jason-todd.herokuapp.com/&client_id=1659663484497725&response_type=code&scope=user_profile,user_media""")
     
+# @app.route('/callback')
+# def remake():
+#     url = "https://api.instagram.com/oauth/access_token"
+#     client_id = "1659663484497725"
+#     client_secret = "bbd2b3d2eca79ca839f29f4d070cff21"
+#     grant_type = "authorization_code"
+#     redirect_uri = "https://jason-todd.herokuapp.com/"
+#     code = "AQDtoc22IdrjcoFDoxh6UnYjUmUReSSjH7lt8FT-XakFHCu7d_lBrJmO6U8kmDDUh7gTxN8S4o4-p7U4XYNFYV3_A3TGxpoSMiJPX_b8GJ3H8huVRAOOZTjIwrYjlOOAdbjuJxrEnsKD1EUMkbyHFa7RawjifHWzRo5jBJEJNxgygdRGDIlvgNVAnsvRiCRGgLi8rc3_sgC5vWIZ2HLJdNxO85__VpcD78FQ3-6vgGv9Gg"
+
+#     # Faça a solicitação POST
+#     data = {
+#         "client_id": client_id,
+#         "client_secret": client_secret,
+#         "grant_type": grant_type,
+#         "redirect_uri": redirect_uri,
+#         "code": code
+#     }
+#     response = requests.post(url, data=data)
+
+#     """
+#     Jason bot
+#     """
+
+#     return response.text
+
 
 SENDER_EMAIL = os.environ['EMAIL_SENDER']
 RECEIVER_EMAIL = os.environ['EMAIL_RECEIVER']
@@ -88,4 +113,4 @@ def run():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
