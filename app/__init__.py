5475056efa84e48901ec6fa7767659175eae1c53
Gian_Pereira <gian.21.otaku@gmail.com>
2024-11-20 22:44:42 -0300

    new try

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from logging import INFO
 from flask_cors import CORS
 import secrets 
-
+# ESTE CODIGO FOI CRIADO COM BASTANTE RAIVA
 
 def create_app():
     app = Flask(__name__)
     cors = CORS(app)
     app.logger.level = INFO
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jason.db'
+    cors.init_app(app)
 
     app.secret_key = secrets.token_hex(50)
     print(app.secret_key)
